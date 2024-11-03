80f1a101ced882cc8b9d35ebd3877df4b03ca453
Letícia Pitta <leticia.ipynb@gmail.com>
2024-11-02 22:37:27 -0300

    Fix - Apagando imports desnecessários e finalizando forms com predição de possível assinante

--- possiveis_assinantes/app.py
+++ possiveis_assinantes/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session
 import pandas as pd
 import pickle
 from sklearn.metrics import *
